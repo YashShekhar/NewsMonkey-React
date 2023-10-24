83f2e74a3522f8e6fe5853ee8161aa763f4e34ce
Yash Shekhar <yashshekhar28@gmail.com>
2023-10-24 17:46:17 +0530

    Initial ready version of NewsMonkey

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Navbar from './components/Navbar'
+import News from './components/News'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  pageSize = 12;
+  render() {
+    return (
+      <div>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route exact path="/" element={<News key='general' country='in' pageSize={this.pageSize} category='general' />}></Route>
+            <Route exact path="/business" element={<News key='business' country='in' pageSize={this.pageSize} category='business' />}></Route>
+            <Route exact path="/entertainment" element={<News key='entertainment' country='in' pageSize={this.pageSize} category='entertainment' />}></Route>
+            <Route exact path="/general" element={<News key='general' country='in' pageSize={this.pageSize} category='general' />}></Route>
+            <Route exact path="/health" element={<News key='health' country='in' pageSize={this.pageSize} category='health' />}></Route>
+            <Route exact path="/science" element={<News key='science' country='in' pageSize={this.pageSize} category='science' />}></Route>
+            <Route exact path="/sports" element={<News key='sports' country='in' pageSize={this.pageSize} category='sports' />}></Route>
+            <Route exact path="/technology" element={<News key='technology' country='in' pageSize={this.pageSize} category='technology' />}></Route>
+          </Routes>
+        </Router>
+      </div>
+    )
+  }
 }
 
-export default App;
---
+++ src/components/NewsItem.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+
+    render() {
+        let { title, description, imageUrl, newsUrl, author, date, source } = this.props;
+        return (
+            <div className='my-3'>
+                <div className="card">
+                    <span className="position-absolute top-0 translate-middle badge rounded-pill bg-dark" style={{ zIndex: '1', left: '90%' }}>{source}</span>
+                    <img src={imageUrl ? imageUrl : "https://thumbs.dreamstime.com/b/news-woodn-dice-depicting-letters-bundle-small-newspapers-leaning-left-dice-34802664.jpg"} className="card-img-top" alt={imageUrl} />
+                    <div className="card-body">
+                        <h5 className="card-title">{title}</h5>
+                        <p className="card-text">{description}</p>
+                        <p className="card-text"><small className="text-muted">By {author ? author : 'Unknown'} on {new Date(date).toUTCString()}</small></p>
+                        <a rel="noreferrer" href={newsUrl} target='_blank' className="btn btn-sm btn-dark">Read more..</a>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default NewsItem
